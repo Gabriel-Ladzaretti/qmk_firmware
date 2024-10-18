e3e6c5b5a933877a8a4aa7ce05c288eb914fc2d7
Gabriel Ladzaretti <gabriel.ladzaretti@gmail.com>
2024-10-19 00:50:30 +0300

    add tap dance + del key override

--- keyboards/gmmk/pro/rev1/ansi/keymaps/Gabriel_Ladzaretti/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/Gabriel_Ladzaretti/keymap.c
@@ -14,13 +14,28 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include QMK_KEYBOARD_He
+#include QMK_KEYBOARD_H
 
+// Layer Definitions
 enum layer_names {
     _BL,
     _FL,
     _NL,
 };
+
+// Tap Dance declarations
+enum {
+    TD_END_HOME,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for End, twice for Home
+    [TD_END_HOME] = ACTION_TAP_DANCE_DOUBLE(KC_END, KC_HOME),
+};
+
+// Key Override Definitions
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -53,12 +68,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //  Sh_L                   Z        X        C        V        B        N        M        ,        .        ?                 Sh_R     Up       End
     //  Ct_L          Win_L    Alt_L                               SPACE                               Alt_R    FN       Ct_R     Left     Down     Right
     [_BL] = LAYOUT(
-        KC_ESC,       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
+        KC_ESC,       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_MUTE,
         KC_GRV,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
         KC_TAB,       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-        LT(2,KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
+        TG(_NL),      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
         KC_LSFT,               KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL,      KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL,      KC_LGUI, KC_LALT,                            KC_SPC,                             KC_APP, MO(1),   KC_PSCR, KC_LEFT, KC_DOWN,  KC_RGHT
     ),
 
 
@@ -77,20 +92,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,      _______, _______,                            _______,                            KC_RALT, _______, KC_RCTL, RGB_SPD, RGB_RMOD, RGB_SPI
     ),
 
-    //  ESC           F1         F2         F3         F4         F5         F6         F7         F8         F9         F10        F11        F12	      Prt               Rotary(Mute)
-    //  ~             1          2          3          4          5          6          7          8          9          0           -         (=)	      BackSpc               Del
-    //  Tab           Q          W          E          R          T          Y          U          I          O          P          [          ]          \                     PgUp
-    //  Caps          A          S          D          F          G          H          J          K          L          ;          "                     Enter                 PgDn
-    //  Sh_L                     Z          X          C          V          B          N          M          ,          .          ?                     Sh_R       Up         End
-    //  Ct_L          Win_L      Alt_L                                       SPACE                                       Alt_R      FN         Ct_R       Left       Down       Right
+    //  ESC           F1         F2         F3         F4         F5               F6         F7         F8         F9         F10        F11        F12	      Prt               Rotary(Mute)
+    //  ~             1          2          3          4          5                6          7          8          9          0           -         (=)	      BackSpc               Del
+    //  Tab           Q          W          E          R          T                Y          U          I          O          P          [          ]          \                     PgUp
+    //  Caps          A          S          D          F          G                H          J          K          L          ;          "                     Enter                 PgDn
+    //  Sh_L                     Z          X          C          V                B          N          M          ,          .          ?                     Sh_R       Up         End
+    //  Ct_L          Win_L      Alt_L                                             SPACE                                       Alt_R      FN         Ct_R       Left       Down       Right
     [_NL] = LAYOUT(
-        _______,      XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   _______,              _______, 
-        _______,      XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_END,    XXXXXXX,   KC_HOME,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   _______,              _______, 
-        _______,      XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_PGUP,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   _______,              _______, 
-        _______,      XXXXXXX,   XXXXXXX,   KC_PGDN,   XXXXXXX,   XXXXXXX,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   XXXXXXX,   XXXXXXX,              _______,              _______,          
-        _______,                 XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,              _______,   XXXXXXX,   _______, 
-        _______,      _______,   _______,                                    _______,                                    _______,   _______,   _______,   XXXXXXX,   XXXXXXX,   XXXXXXX
+        _______,      XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   _______,              _______, 
+        _______,      XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_END,    XXXXXXX,         KC_HOME,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   _______,              _______, 
+        _______,      XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   KC_PGUP,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   _______,              _______, 
+        TG(_NL),      XXXXXXX,   XXXXXXX,   KC_PGDN,   XXXXXXX,   TD(TD_END_HOME), KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   XXXXXXX,   XXXXXXX,              _______,              _______,          
+        _______,                 XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,              _______,   XXXXXXX,   _______, 
+        _______,      _______,   _______,                                          _______,                                    _______,   _______,   _______,   XXXXXXX,   XXXXXXX,   XXXXXXX
     )
+};
+
+// Globally defines all key overrides
+const key_override_t *key_overrides[] = {
+	&delete_key_override,
 };
 
 #if defined(ENCODER_MAP_ENABLE)
